8e431dbeb3e070bea97d6dd85c7e7b8dacaa9506
kechigon <kekekekeigorou@gmail.com>
2021-01-30 03:02:51 +0900

    UPDATE: ft_strjoin.c

--- Libft/ft_strjoin.c
+++ Libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: kkurita <kkurita@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/25 18:15:43 by kkurita           #+#    #+#             */
-/*   Updated: 2021/01/30 03:00:55 by kkurita          ###   ########.fr       */
+/*   Updated: 2021/01/30 03:02:37 by kkurita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,6 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	/*char	*str;
-	char	*head;
-	size_t	len;
-
-	len = ft_strlen(s1) + ft_strlen(s2);
-	if (!(str = (char *)malloc(sizeof(char) * len + 1)))
-		return (0);
-	head = str;
-	while (*s1)
-		*str++ = *s1++;
-	while (*s2)
-		*str++ = *s2++;
-	*str = '\0';
-	return (head);*/
 	char	*cpy;
 	size_t	s1len;
 	size_t	s2len;
@@ -39,5 +25,5 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	ft_memcpy(cpy, s1, s1len);
 	ft_memcpy(cpy + s1len, s2, s2len);
 	*(cpy + s1len + s2len + 1) = '\0';
-	return (cpy);*/
+	return (cpy);
 }
