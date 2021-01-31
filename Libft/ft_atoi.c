ee904d4424607f99991c8c424369a1d482e91155
kechigon <kekekekeigorou@gmail.com>
2021-02-01 03:26:23 +0900

    UPDATE: ft_atoi.c

--- Libft/ft_atoi.c
+++ Libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: kkurita <kkurita@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/24 14:11:50 by kkurita           #+#    #+#             */
-/*   Updated: 2021/01/24 17:49:36 by kkurita          ###   ########.fr       */
+/*   Updated: 2021/02/01 03:18:46 by kkurita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,9 @@ int	ft_atoi(const char *s)
 {
 	char sign;
 	long ans;
-	long ov_div;
-	long ov_mod;
 
 	sign = 1;
 	ans = 0;
-	ov_div = LONG_MAX / 10;
-	ov_mod = LONG_MAX % 10;
 	while (('\t' <= *s && *s <= '\r') || *s == ' ')
 		s++;
 	if (*s == '-' || *s == '+')
@@ -32,8 +28,6 @@ int	ft_atoi(const char *s)
 	}
 	while (ft_isdigit(*s))
 	{
-		if (ans > ov_div || (ans == ov_div && *s > ov_mod))
-			return ((int)LONG_MAX);
 		ans = ans * 10 + (*s++ - '0');
 	}
 	return ((int)ans * sign);
